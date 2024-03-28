212f584bcb8d56b8f211497be174418ce1569ae6
Igor Sechinov <112351537+igasin@users.noreply.github.com>
2024-03-28 14:14:12 +0300

    feat(weather-app): add styles and api

--- src/components/weatherapp/WeatherApp.tsx
+++ src/components/weatherapp/WeatherApp.tsx
@@ -1,8 +1,44 @@
-import cloud_icon from '../../assets/cloud.png'
+import { useState } from 'react'
+import humidity_icon from '../../assets/humidity.png'
 import search_icon from '../../assets/search.png'
+import wind_icon from '../../assets/wind.png'
 import styles from './Weather.module.scss'
 
 export const WeatherApp = () => {
+	const OPENWEATHERMAP_API_KEY = 'd48cd88a49b1128f4207663139c352fb'
+
+	const [city, setCity] = useState('')
+
+	const [weatherData, setWeatherData] = useState({
+		humidity: '',
+		wind: '',
+		temperature: '',
+		location: '',
+		icon: '',
+	})
+
+	const search = async () => {
+		if (city === '') {
+			return
+		}
+
+		const URL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=Metric&appid=${OPENWEATHERMAP_API_KEY}`
+
+		const response = await fetch(URL)
+		const data = await response.json()
+
+		const iconUrl =
+			'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
+
+		setWeatherData({
+			humidity: data.main.humidity,
+			wind: data.wind.speed,
+			temperature: data.main.temp,
+			location: data.name,
+			icon: iconUrl,
+		})
+	}
+
 	return (
 		<div className={styles.container}>
 			<div className={styles.topBar}>
@@ -10,15 +46,37 @@ export const WeatherApp = () => {
 					type='text'
 					className={styles.cityInput}
 					placeholder='Search city'
+					onChange={e => setCity(e.target.value)}
 				/>
-				<div className={styles.searchIcon}>
+				<div className={styles.searchIcon} onClick={search}>
 					<img src={search_icon} alt='' />
 				</div>
-				<div className={styles.weatherImage}>
-					<img src={cloud_icon} alt='' />
+				<div />
+			</div>
+			<div className={styles.weatherImage}>
+				<img src={weatherData.icon} alt='' />
+			</div>
+			<div className={styles.weatherTemperature}>
+				{weatherData.temperature}°
+			</div>
+			<div className={styles.weatherLocation}>{weatherData.location}</div>
+			<div className={styles.dataContainer}>
+				<div className={styles.element}>
+					<img src={humidity_icon} alt='' className={styles.icon} />
+					<div className={styles.data}>
+						<div className={styles.humidityPercent}>
+							{weatherData.humidity}%
+						</div>
+						<div className={styles.text}>Humidity</div>
+					</div>
 				</div>
-				<div className={styles.weatherTemperature}>24°</div>
-				<div className={styles.weatherLocation}>London</div>
+				<div className={styles.element}>
+					<img src={wind_icon} alt='' className={styles.icon} />
+					<div className={styles.data}>
+						<div className={styles.windRate}>{weatherData.wind} km/h</div>
+						<div className={styles.text}>Wind</div>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
