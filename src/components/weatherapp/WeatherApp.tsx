5cd368f6ef05c88490350084a147be99a2c451e0
Igor Sechinov <112351537+igasin@users.noreply.github.com>
2024-03-28 15:46:54 +0300

    feat(weather-app): modify styles

--- src/components/weatherapp/WeatherApp.tsx
+++ src/components/weatherapp/WeatherApp.tsx
@@ -28,12 +28,12 @@ export const WeatherApp = () => {
 		const data = await response.json()
 
 		const iconUrl =
-			'https://openweathermap.org/img/wn/' + data.weather[0].icon + '.png'
+			'https://openweathermap.org/img/wn/' + data.weather[0].icon + '@2x.png'
 
 		setWeatherData({
 			humidity: data.main.humidity,
 			wind: data.wind.speed,
-			temperature: data.main.temp,
+			temperature: data.main.temp.toFixed(1),
 			location: data.name,
 			icon: iconUrl,
 		})
@@ -47,6 +47,7 @@ export const WeatherApp = () => {
 					className={styles.cityInput}
 					placeholder='Search city'
 					onChange={e => setCity(e.target.value)}
+					onKeyDown={e => e.key === 'Enter' && search()}
 				/>
 				<div className={styles.searchIcon} onClick={search}>
 					<img src={search_icon} alt='' />
@@ -65,16 +66,16 @@ export const WeatherApp = () => {
 					<img src={humidity_icon} alt='' className={styles.icon} />
 					<div className={styles.data}>
 						<div className={styles.humidityPercent}>
-							{weatherData.humidity}%
+							{weatherData.humidity} %
 						</div>
-						<div className={styles.text}>Humidity</div>
+						{/* <div className={styles.text}>Humidity</div> */}
 					</div>
 				</div>
 				<div className={styles.element}>
 					<img src={wind_icon} alt='' className={styles.icon} />
 					<div className={styles.data}>
 						<div className={styles.windRate}>{weatherData.wind} km/h</div>
-						<div className={styles.text}>Wind</div>
+						{/* <div className={styles.text}>Wind</div> */}
 					</div>
 				</div>
 			</div>
